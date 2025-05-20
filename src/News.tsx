717d0ef436b299fc9f8de8876ea7c34b96c84d24
Yannis Febvre <yannisfebvre@gmail.com>
2025-05-20 16:41:22 +0200

    qsd

--- src/News.tsx
+++ src/News.tsx
@@ -138,7 +138,7 @@ const News = () => {
             options={{
               type: 'slide',
               perPage: 2.5,
-              gap: '1rem',
+              gap: '1.5rem',
               pagination: false,
               arrows: false,
               drag: 'free',
